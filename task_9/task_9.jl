6848b704798d4d0d324754cb5cfa6bb90a5feef6
boryaa <pashkovborya@gmail.com>
2020-12-17 22:50:43 +0300

    solution for task_9

--- task_9/task_9.jl
+++ task_9/task_9.jl
@@ -1,25 +1,22 @@
 function find_marker!(r::Robot)
     num_steps = 1
-    while !ismarker(r::Robot)
-        moves!(r, Nord, num_steps)
-        moves!(r, West, num_steps)
-        num_steps += 1
-        moves!(r, Sud, num_steps)
-        moves!(r, Ost, num_steps)
+    side = Nord
+    while !ismarker(r)
+        for _ in 1:2
+            check_mark!(r, side, num_steps)
+            side = HorizonSide(mod(Int(side)+3,4))
+        end
         num_steps += 1
     end
-
 
 end
 
 
-function moves!(r::Robot, side::HorizonSide, num_steps::Int)
-    if !ismarker(r::Robot)
-        for i in 1:num_steps
-            move!(r, side)
-            if ismarker(r::Robot)
-                break
-            end
+function check_mark!(r::Robot, side::HorizonSide, num_steps::Int)
+    for _ in 1:num_steps
+        move!(r,side)
+        if ismarker(r)
+            break
         end
     end
 end
